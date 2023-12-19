ced2271fb33b96de8e0e24b380ddf0eeac6912c7
tianxueren <1770664399@qq.com>
2023-12-19 10:57:55 +0800

    add: test

--- src/main/java/com/itxve/graalvmnativedemo/GraalvmNativeDemoApplication.java
+++ src/main/java/com/itxve/graalvmnativedemo/GraalvmNativeDemoApplication.java
@@ -7,19 +7,41 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ServerWebExchange;
 
+import java.io.IOException;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Date;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 @SpringBootApplication
 @RestController
 @Slf4j
 public class GraalvmNativeDemoApplication {
 
-  @RequestMapping("/")
-  String home(ServerWebExchange exchange) {
-    log.info("{}", exchange.getRequest().getHeaders());
-    return "Hello World!";
-  }
+	@RequestMapping("/")
+	String hello() {
+		return "Hello,Now :" + new Date();
+	}
 
-  public static void main(String[] args) {
-    SpringApplication.run(GraalvmNativeDemoApplication.class, args);
-  }
+		@RequestMapping("/dir/list")
+	List<Path> home(ServerWebExchange exchange) {
+		String path = exchange.getRequest().getQueryParams().getFirst("path");
+		try {
+			Stream<Path> list = Files.list(Path.of(path));
+			return list.collect(Collectors.toList());
+		} catch (IOException e) {
+			System.out.println(e);
+			return Collections.emptyList();
+		}
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(GraalvmNativeDemoApplication.class, args);
+	}
 
 }
